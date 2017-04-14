f493589fef8fb00888855268a093d60b7a3932eb
wang311 <1216417625@qq.com>
2017-04-14 15:37:20 +0800

    stack

--- DataStructures/Stack.js
+++ DataStructures/Stack.js
@@ -3,31 +3,40 @@ function stack(){
     this.top = 0;
     this.push = push;
     this.pop = pop;
-    this.peek = peek; //只返回栈顶元素，不删除元素
+    this.peek = peek; 
     this.length = length;
 }
 
+/*增加一个元素*/
+/*根据栈的特点，增加就是压进一个元素，就是在栈顶push一个*/
 function push(element){
     this.dataStore[this.top++] = element;
 }
 
+/*减少一个元素*/
+/*根据栈的特点，减少就是蹦出一个元素，就是在栈顶pop一个*/
 function pop(element){
     return this.dataStore[--this.top];
 }
 
+/*只返回栈顶元素，不删除元素*/
 function peek(){
     return this.dataStore[this.top-1]
 }
 
+/*返回栈的长度*/
+/*根据栈的特点，返回栈顶的长度*/
 function length(){
     return this.top;
 }
 
+/*清空栈*/
+/*让栈顶的位置为0*/
 function clear(){
     this.top = 0;
 }
 
-/*数之间的相互转换*/
+/*不同数制之间的相互转换*/
 function mulBase(num,base){
     var s = new stack();
     while(num > 0){
